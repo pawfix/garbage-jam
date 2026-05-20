ee14b19d5d157441e80b91dc300870a11cc1fea7
Misiek1245 <embappe7@gmail.com>
2026-05-20 14:40:17 +0200

    add dubble jump etc.

--- script/player/player_body.gd
+++ script/player/player_body.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var usedDoubleJump: bool = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,6 +13,13 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		#Text.createTextBox("Test Box", "res://assets/sprites/player/sprite.tres")
+	
+	if Input.is_action_just_pressed("jump") and !is_on_floor() and Data.inventory.double_jump and !usedDoubleJump:
+		velocity.y += JUMP_VELOCITY
+		usedDoubleJump = true
+	if usedDoubleJump and is_on_floor():
+		usedDoubleJump = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
